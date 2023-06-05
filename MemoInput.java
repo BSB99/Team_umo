9c3a04c9264e6e1d9ba751d57550f9a1d6ed45ab
SeungBum Baek <wer06099@naver.com>
2023-06-05 09:53:09 +0900

    종료버튼 활성화

---
+++ MemoInput.java
@@ -0,0 +1,38 @@
+package team_memo;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+
+public class MemoInput {
+        //이름, 제목, 내용, 비밀번호를 입력 받아야 한다.
+    private ArrayList<Memo> memos;
+    private int contentNumber;
+    public MemoInput(){
+        memos = new ArrayList<>();
+        contentNumber = 0;
+    }
+    public void input() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("이름");
+        String name = sc.nextLine();
+
+        System.out.println("제목");
+        String title = sc.nextLine();
+
+        System.out.println("내용");
+        String memoContent = sc.nextLine();
+
+        System.out.println("비밀 번호");
+        int passWord = sc.nextInt();
+
+        LocalDateTime localDateTime = LocalDateTime.now();
+
+        memos.add(new Memo(++contentNumber, name, title, memoContent, passWord, localDateTime));
+    }
+
+    public List<Memo> getMemos() {
+            return memos;
+    }
+}
